4f5e36b7709253f9da89f69c81d110338db6d13f
med1seghir5 <abdelmalek.medseghir@gmail.com>
2023-12-26 07:14:55 +0400

    feat: added home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Header from "@/components/Header";
+import Header from "@/components/Bubbles";
+import Hero from "@/components/Hero";
+
 
 export default function HomePage() {
   return (
     <main>
       <Header />
+      <Hero />
     </main>
   );
 }
