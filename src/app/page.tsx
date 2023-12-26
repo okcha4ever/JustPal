677267cbf1436c362a086edb007f2cdb1ee53920
med1seghir5 <abdelmalek.medseghir@gmail.com>
2023-12-26 08:49:11 +0400

    feat: added sign-up page (not finished)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/Hero";
 
 export default function HomePage() {
   return (
-    <main className="overflow-y-hidden py-10">
+    <main className="overflow-y-hidden py-10 bg-[#F5F5F5]">
       <Bubbles />
       <Hero />
     </main>
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,8 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <section className='flex items-center justify-center pt-7'>
+      <div>
+            <Image width={400} height={400} alt='logo img' src='/Logo.svg' className='bg-transparent'/>
+      </div>        
+
+
+    </section>
   )
 }
 
