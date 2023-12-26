98bcf9bb55e533a257be951a003b089c3f7e7927
okcha4ever <minoukhiari4@gmail.com>
2023-12-26 05:44:04 +0100

    chore: some adjustments in home/landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import Bubbles from "@/components/Bubbles";
 import Hero from "@/components/Hero";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <main className="overflow-y-hidden py-10">
       <Bubbles />
       <Hero />
+      <Link
+        href="/more"
+        className="flex items-center justify-center text-lg font-light text-blue-600 underline"
+      >
+        Check some people's stories...
+      </Link>
     </main>
   );
 }
