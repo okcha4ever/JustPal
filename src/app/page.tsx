ed3abed9a76ef788a09336063e7fad27d558bcf0
okcha4ever <minoukhiari4@gmail.com>
2023-12-26 05:29:11 +0100

    feat: added ai image and checking if it exists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Header from "@/components/Header";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
     <main>
       <Header />
+      <Image
+        src="https://processed-model-result.s3.us-east-2.amazonaws.com/40f6fe15-25f6-4616-b044-b50ecb01ed5c_0.png"
+        alt="some"
+        width={400}
+        height={400}
+      />
     </main>
   );
 }
